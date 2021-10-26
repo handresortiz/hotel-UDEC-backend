09eb8706d29b25bd2299d8a4ff738a8e420da038
Daniel-Cas <dmattcomics@gmail.com>
2021-10-26 09:02:43 -0500

    Microservice email created whitout config

---
+++ Hotel-UDEC/dominionegocio/microservice-email/src/main/java/co/edu/ucundinamarca/negocio/microservice/email/controllers/EmailController.java
@@ -0,0 +1,12 @@
+package co.edu.ucundinamarca.negocio.microservice.email.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/mail")
+public class EmailController {
+
+
+
+}
---
+++ Hotel-UDEC/dominionegocio/microservice-email/src/test/java/co/edu/ucundinamarca/negocio/microservice/email/MicroserviceEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.ucundinamarca.negocio.microservice.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceEmailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
