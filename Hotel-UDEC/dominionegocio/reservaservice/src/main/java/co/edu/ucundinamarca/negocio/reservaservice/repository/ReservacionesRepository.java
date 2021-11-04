e9fe1e4448c34a558264416fcc6239ccffc0e4df
Johan Romero <johan.romero5879@gmail.com>
2021-11-03 22:39:01 -0500

    Ajuste del filtro habitaciones disponibles
    Se modifico la query que indica si la habitación está o no disponible según rango de fechas

--- Hotel-UDEC/dominionegocio/reservaservice/src/main/java/co/edu/ucundinamarca/negocio/reservaservice/repository/ReservacionesRepository.java
+++ Hotel-UDEC/dominionegocio/reservaservice/src/main/java/co/edu/ucundinamarca/negocio/reservaservice/repository/ReservacionesRepository.java
@@ -5,14 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.Date;
-import java.util.Optional;
+import java.util.List;
 
 public interface ReservacionesRepository extends JpaRepository<Reservaciones, Integer> {
 
-    @Query( "SELECT r FROM Reservaciones r WHERE r.habitacion.id_habitacion = ?1" )
-    Optional<Reservaciones[]> findByHabitacion( Integer id );
+    @Query("SELECT r FROM Reservaciones r WHERE r.habitacion.id_habitacion = ?1")
+    List<Reservaciones> findByHabitacion(Integer id );
 
     @Query( "SELECT CASE WHEN count(r) = 0 THEN TRUE ELSE FALSE END FROM Reservaciones r " +
-            "WHERE r.habitacion.id_habitacion = ?1 AND (r.fec_inicio >= ?2 AND r.fec_fin <= ?3 )" )
+            "WHERE r.habitacion.id_habitacion = ?1 " +
+            "AND ((r.fec_inicio BETWEEN ?2 AND ?3) OR (r.fec_fin BETWEEN ?2 AND ?3))" )
     Boolean esHabitacionDisponible(Integer id_habitacion, Date fec_inicio, Date fec_fin );
 }
