220febf109f185ca2d908d80bf9af6d7ff0ba121
Daniel-Cas <dmattcomics@gmail.com>
2021-11-23 00:34:57 -0500

    Chart reservation finished

--- Hotel-UDEC/dominionegocio/chartsService/src/main/java/co/edu/ucundinamarca/negocio/chartsservice/entities/Cuenta.java
+++
@@ -1,2 +0,0 @@
-package co.edu.ucundinamarca.negocio.chartsservice.entities;public class Cuenta {
-}
--- Hotel-UDEC/dominionegocio/chartsService/src/main/java/co/edu/ucundinamarca/negocio/chartsservice/entities/Reservacion.java
+++ Hotel-UDEC/dominionegocio/chartsService/src/main/java/co/edu/ucundinamarca/negocio/chartsservice/entities/Reservacion.java
@@ -1,4 +1,35 @@
 package co.edu.ucundinamarca.negocio.chartsservice.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Entity
+@Table( name = "reservaciones" )
 public class Reservacion {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Integer id_reservacion;
+
+    @NotNull
+    private Long valor;
+
+    @NotNull
+    private Date fec_inicio;
+
+    @NotNull
+    private Date fec_fin;
+
+    private Date fec_cambio;
+
+    private Long id_usuario_cambio;
+
+    @NotNull
+    private Integer habitacion;
+
+    @NotNull
+    private Integer cuenta;
 }
